c2ad7482946cffe3248fe2ad817a8dc45abf0948
Estudante Alura <105929920+Anaketlin@users.noreply.github.com>
2024-08-15 09:31:28 -0300

    .

--- script.js
+++ script.js
@@ -93,10 +93,13 @@ function mostraPergunta() {
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
       const botaoAlternativas = document.createElement("button");
-      botaoAlternativas.textContent = alternativa;
+      botaoAlternativas.textContent = alternativa.texto;
+      botaoAlternativas.addEventListener("click", function () {
+        atual++;
+        mostraPergunta();
+      });
       caixaAlternativas.appendChild(botaoAlternativas);
     }
   }
-
 mostraPergunta();
 
